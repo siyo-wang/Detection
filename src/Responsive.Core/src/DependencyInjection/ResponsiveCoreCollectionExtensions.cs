b6c9b77eb0e0c68a4fd841302f46dead812acbfd
Sarin Na Wangkanai <sarin@wangkanai.com>
2019-03-07 12:40:55 +0700

    project reference

---
+++ src/Responsive.Core/src/DependencyInjection/ResponsiveCoreCollectionExtensions.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ResponsiveCoreCollectionExtensions
+    {
+    }
+}
